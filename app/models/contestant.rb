b9240c888f3eb1c739a813389d2238bee68f3d2c
Grace Joh <graceehjoh@gmail.com>
2023-04-23 11:53:57 -0500

    feat/test: Contestant season number and description

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,12 @@ class Contestant < ApplicationRecord
   belongs_to :bachelorette
   has_many :contestant_outings
   has_many :outings, through: :contestant_outings
+
+  def season_number
+    bachelorette.season_number
+  end
+
+  def season_description
+    bachelorette.season_description
+  end
 end
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Contestant, type: :model do
     it { should have_many :contestant_outings }
     it { should have_many(:outings).through(:contestant_outings) }
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      test_data
+    end
+
+    describe 'season_number' do
+      it 'returns its bachelorettes season number' do
+        expect(@pete.season_number).to eq(@bach1.season_number)
+        expect(@scott.season_number).to eq(@bach2.season_number)
+      end
+    end
+
+    describe 'season_description' do
+      it 'returns its bachelorettes season description' do
+        expect(@pete.season_description).to eq(@bach1.season_description)
+        expect(@scott.season_description).to eq(@bach2.season_description)
+      end
+    end
+  end
 end
